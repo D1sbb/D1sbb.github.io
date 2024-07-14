01e0e0a3ede3472917fde9313f9e86c130c0a3ae
D1sbb <D1sbb>
2024-07-14 19:55:37 +0800

    up

--- assets/toc.js
+++ assets/toc.js
@@ -57,7 +57,7 @@
 
     var level = get_level(headers[0]),
       this_level,
-      html = settings.title + " <" +settings.listType + " class=\"" + settings.classes.list +"\">";
+      html = settings.title + " <" +settings.listType + " class=floating-ul\"" + settings.classes.list +"\">";
     headers.on('click', function() {
       if (!settings.noBackToTopLinks) {
         window.location.hash = this.id;
@@ -79,7 +79,7 @@
       }
       else if (this_level > level) { // lower level than before; expand the previous to contain a ol
         for(i = this_level; i > level; i--) {
-          html += "<" + settings.listType + " class=\"" + settings.classes.list +"\">" +
+          html += "<" + settings.listType + " class=floating-ul\"" + settings.classes.list +"\">" +
                   "<li class=\"" + settings.classes.item + "\">"
         }
         html += createLink(header);
